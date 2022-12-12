be809e317cb795cfb5d64b32c81b69f059aab6a4
wane <36815574+wane199@users.noreply.github.com>
2022-12-12 10:35:38 +0800

    1212

--- BLS_EP_files/pet&wordcloud.R
+++ BLS_EP_files/pet&wordcloud.R
@@ -6,24 +6,24 @@ library(occ)
 data(occ.example)
 occ.example
 
-m = occ(occ.example)
+m <- occ(occ.example)
 
-print(m)     # Prints the neuroreceptor occupancy coefficients
+print(m) # Prints the neuroreceptor occupancy coefficients
 
-summary(m)   # Also prints the non-displaceable volume of
+summary(m) # Also prints the non-displaceable volume of
 
-fitted(m)    # Prints the fitted values
+fitted(m) # Prints the fitted values
 
 residuals(m) # Prints the residuals
 
-plot(m)      # Plots the estimated and observed volumes of
+plot(m) # Plots the estimated and observed volumes of
 # distribution
 
 
 library(oro.pet)
 n <- 11
-h <- seq(200, 150, length=n)
-w <- seq(80, 120, length=n)
+h <- seq(200, 150, length = n)
+w <- seq(80, 120, length = n)
 cbind(h, w, leanBodyMass(h, w, "male"), leanBodyMass(h, w, "female"))
 
 
@@ -36,31 +36,34 @@ totalSUV(suv, mask, z, bg, local = TRUE)
 
 #############################
 # wordcloud
-options("repos" = c(CRAN="https://mirrors.tuna.tsinghua.edu.cn/CRAN/")) # 设置清华镜像
-install.packages("jsonlite")    #一定要安装这个包
-library('devtools')
+options("repos" = c(CRAN = "https://mirrors.tuna.tsinghua.edu.cn/CRAN/")) # 设置清华镜像
+install.packages("jsonlite") # 一定要安装这个包
+library("devtools")
 devtools::install_github("lchiffon/wordcloud2")
 
 # library
-library(wordcloud2) 
-letterCloud( demoFreq, word = "THANKS PET!", color='random-light' , backgroundColor="black")
-my_graph <- letterCloud( demoFreq, word = "PET", color="white", backgroundColor="pink")
+library(wordcloud2)
+letterCloud(demoFreq, word = "THANKS PET!", color = "random-light", backgroundColor = "black")
+my_graph <- letterCloud(demoFreq, word = "PET", color = "white", backgroundColor = "pink")
+my_graph
 
 # Export the wordcloud
 # Wordcloud2 is a html widget. It means your wordcloud will be output in a HTML format.You can export it as a png image using rstudio, or using the webshot library as follow:
 
 # load wordcloud2
-library(wordcloud2) 
+library(wordcloud2)
 # install webshot
 library(webshot)
 webshot::install_phantomjs()
 
 # Make the graph
-my_graph <- wordcloud2(demoFreq, size=1.5)
+my_graph <- wordcloud2(demoFreq, size = 1.5)
 
 # save it in html
 library("htmlwidgets")
-saveWidget(my_graph,"./BLS_EP_files/tmp.html",selfcontained = F)
+saveWidget(my_graph, "./BLS_EP_files/tmp.html", selfcontained = F)
 
 # and in png or pdf
-webshot("./BLS_EP_files/tmp.html","./BLS_EP_files/fig_1.pdf", delay =5, vwidth = 480, vheight=480)
+webshot("./BLS_EP_files/tmp.html", "./BLS_EP_files/fig_1.pdf", delay = 5, vwidth = 480, vheight = 480)
+
+
