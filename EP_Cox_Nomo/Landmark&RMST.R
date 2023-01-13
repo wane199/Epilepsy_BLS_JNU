7cc831260719fd6e9b47a63dc4bcdff6d9444d15
wane199 <wane199@outlook.com>
2023-01-13 21:33:38 +0800

    0113

--- EP_Cox_Nomo/Landmark&RMST.R
+++ EP_Cox_Nomo/Landmark&RMST.R
@@ -1,9 +1,11 @@
 # [生存分析之landmark](https://mp.weixin.qq.com/s?__biz=Mzg2OTY4NzAxNA==&mid=2247483674&idx=1&sn=b71b104e19dc5ee688b2d4c9733b9695&chksm=ce980a93f9ef83859827e1c4fa89c8f2c9a8ec08819060d5a04a356d69535a43bdde9395f74e&mpshare=1&scene=1&srcid=0113tqJAW7uPgIqaH9kLSHrh&sharer_sharetime=1673612760201&sharer_shareid=13c9050caaa8b93ff320bbf2c743f00b#rd)
-# [生存分析5：限制平均生存时间 （Restricted Mean Survival Time,RMST）](https://mp.weixin.qq.com/s?__biz=MzkyNTIyNDYwNQ==&mid=2247485393&idx=1&sn=695f8d9a7d2bb12b394cc4c1b8167f82&chksm=c1c89c23f6bf1535764ca7fa18e888d1a57d7b9c98b47eb7359e10aafa5fa82eae17b483fe19&mpshare=1&scene=1&srcid=01132ByPutJJcv2Hv5rmwgvV&sharer_sharetime=1673612698209&sharer_shareid=13c9050caaa8b93ff320bbf2c743f00b#rd)
+# [生存分析5：限制平均生存时间Restricted Mean Survival Time,RMST](https://mp.weixin.qq.com/s?__biz=MzkyNTIyNDYwNQ==&mid=2247485393&idx=1&sn=695f8d9a7d2bb12b394cc4c1b8167f82&chksm=c1c89c23f6bf1535764ca7fa18e888d1a57d7b9c98b47eb7359e10aafa5fa82eae17b483fe19&mpshare=1&scene=1&srcid=01132ByPutJJcv2Hv5rmwgvV&sharer_sharetime=1673612698209&sharer_shareid=13c9050caaa8b93ff320bbf2c743f00b#rd)
+rm(list = ls())
 library(survival)
 
 # 数据集
-data(colon)
+# data(colon)
+colon <- colon
 str(colon)
 
 # 将分层变量rx由3个水平变为2个水平
@@ -34,17 +36,18 @@ library(survRM2)
 library(tidyverse)
 library(survminer)
 
-data<- read_csv("Checkmate057.csv")
-
-fkdt <- tibble(Subject = as.factor(1:10), 
-               Years = sample(4:20, 10, replace = T),
-               censor = sample(c("Censor", rep("Event", 2)), 10, 
+# data<- read_csv("Checkmate057.csv")
+fkdt <- tibble(subject = as.factor(1:10), 
+               time = sample(4:20, 10, replace = T),
+               status = sample(c("Censor", rep("Event", 2)), 10, 
                                replace = T)) 
 
-ggplot(fkdt, aes(Subject, Years)) + 
+fkdt <- transform(colon, subject = as.factor(1:nrow(colon)))
+colnames(fkdt)
+ggplot(fkdt, aes(subject, time)) + 
   geom_bar(stat = "identity", width = 0.5) + 
   geom_point(data = fkdt, 
-             aes(Subject, Years, color = censor, shape = censor), 
+             aes(subject, time, color = factor(status), shape = factor(status)), 
              size = 6) +
   coord_flip() +
   theme_minimal() + 
