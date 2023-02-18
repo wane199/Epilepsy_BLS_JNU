81e6af53fca452181b972841f25f792646f0423a
wane199 <wane199@outlook.com>
2023-02-18 10:30:36 +0800

    0218

--- EP_Cox_Nomo/K_fold_Cox&ROC_batch.R
+++ EP_Cox_Nomo/K_fold_Cox&ROC_batch.R
@@ -342,6 +342,33 @@ plot(roc$FP, roc$TP,
 )
 abline(0, 1)
 
+# https://www.jianshu.com/p/874fbf43308f
+# rm(list = ls())
+# library(survivalROC)
+# rt <- read.table("./indepInput.txt",sep = "\t",row.names = 1,header = T,check.names = F)
+# pdf(file = "./multiROC.pdf",width = 6,height = 6)
+# rt$futime <- rt$futime/365
+# rocCol <- rainbow(ncol(rt)-2)
+# aucText=c()
+# 
+# par(oma=c(0.5,1,0,1),font.lab=1.5,font.axis=1.5)
+# roc=survivalROC(Stime = rt$futime,status = rt$fustat,marker = rt$riskScore,predict.time = 1,method = "KM")
+# plot(roc$FP,roc$TP,
+#      type = "l",xlim = c(0,1),ylim = c(0,1),col=rocCol[1],
+#      xlab="1-Specificity", ylab="Sensitivity",
+#      lwd=2,cex.main=1.3,cex.lab=1.2,cex.axis=1.2,font=1.2)
+# aucText=c(aucText,paste0("risk score"," (AUC=",sprintf("%.3f",roc$AUC),")"))
+# abline(0,1)
+# 
+# j=1
+# for (i in colnames(rt[,3:(ncol(rt)-1)])){
+#   roc=survivalROC(Stime = rt$futime,status = rt$fustat,marker = rt[,i],predict.time = 1,method = "KM")
+#   j=j+1
+#   aucText=c(aucText,paste0(i," (AUC=",sprintf("%.3f",roc$AUC),")"))
+#   lines(roc$FP,roc$TP,type = "l",xlim = c(0,1),ylim = c(0,1),col=rocCol[j],lwd=2)
+# }
+# legend("bottomright",aucText,lwd = 2,bty = "n",col = rocCol)
+# dev.off()
 
 
 ############################################
