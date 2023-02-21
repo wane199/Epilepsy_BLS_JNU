63be4ade0f0c80d774b7408fe15ddb443300441b
wane199 <wane199@outlook.com>
2023-02-21 09:53:12 +0800

    0221

--- BLS_EP_files/EML/DynNomapp/ui.R
+++ BLS_EP_files/EML/DynNomapp/ui.R
@@ -7,7 +7,7 @@ ui = bootstrapPage(fluidPage(theme = shinytheme("slate"),
                                helpText('Press Quit to exit the application'),
                                actionButton('quit', 'Quit'),
                                br(), br(),
-                               img(src = "https://github.com/wane199/Presentation/blob/master/EP/EP_Cox_Nomo/shinyPredict/PT_Brain_20160714100121_3d.gif?raw=true",
+                               img(src = "https://github.com/wane199/Epilepsy_BLS_JNU/blob/1383c7486dbbdcde8379e32de3799f2885d347cd/EP_Cox_Nomo/shinyPredict/PT_Brain_20160714100121_3d.gif?raw=true",
                                    height = 150, width = 180)
     ),
     mainPanel(tabsetPanel(id = 'tabs',
