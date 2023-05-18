0c9bdcc894bc3f4882ca267b11152e33a1859e48
wane199 <wane199@outlook.com>
2023-05-18 23:54:20 +0800

    0518

--- EP_Cox_Nomo/Survival-swimmer plot&KM叠图.R
+++ EP_Cox_Nomo/Survival-swimmer plot&KM叠图.R
@@ -56,6 +56,77 @@ df %>% dplyr::glimpse()
 
 
 
+# 可视化临床试验中常用的游泳图（swimmer plot）(https://mp.weixin.qq.com/s?__biz=MzkzNzMxNjgxMA==&mid=2247487555&idx=1&sn=bd76a790ef77e6e6ad2c19ada8d320ac&chksm=c29008e6f5e781f08f0e4ef892b4e483a477586dcc2fd231240a3fcc8716e70df7de606c6653&scene=21#wechat_redirect)
+
+
+
+
+
+install.packages("swimplot")
+install.packages("ggplot2")
+
+library(swimplot)
+library(ggplot2)
+
+
+
+
+
+
+
+
+
+
+
+#####################################
+# 叠中叠，一行代码实现带有风险表的生存曲线插入子图(https://mp.weixin.qq.com/s?__biz=MzU4OTc0OTg2MA==&mid=2247500015&idx=1&sn=21902096c48ac75b40fa6af7195e289a&chksm=fdca4be4cabdc2f20e8e67760f503a4b3660ed8acfab96ae965b65ca4569295e9102c803e74f&mpshare=1&scene=1&srcid=0513p68Pd85YCXbx50UjJlB7&sharer_sharetime=1684423067245&sharer_shareid=13c9050caaa8b93ff320bbf2c743f00b#rd)
+# 1. 加载数据
+rm(list = ls())
+dt <- read.csv("C:\\Users\\wane1\\Documents\\file\\sci\\cph\\cph2\\TLE220group.csv",sep = ";")
+str(dt)
+
+# 2. 拟合曲线
+# 使用survfit2()函数来拟合曲线。
+library(ggsurvfit)
+fit <- survfit2(Surv(Follow_up_timemon, Rel._in_5yrs == 1) ~ 1, data = dt)
+fit # 可输出部分信息
+
+# 3. 简单生存曲线
+fit %>%
+  ggsurvfit()
+
+# 4. 生存曲线排版
+# 可以在主图上面添加带有风险表的子图。
+p1 <- survfit(Surv(Follow_up_timemon, Rel._in_5yrs == 1) ~ 1, data = dt) %>% 
+  ggsurvfit(linewidth = 0.8, linetype_aes = TRUE, color = "cyan") + 
+  add_risktable(risktable_stats = "n.risk") +
+  add_censor_mark() +
+  add_confidence_interval() +
+  # add_quantile() +
+  theme_classic(base_size = 14) +
+  theme(legend.position=c(0.2,0.2)) +
+  ggsci::scale_fill_nejm()
+p1
+p2 <- survfit(Surv(Follow_up_timemon, Rel._in_5yrs == 1) ~ 1, data = dt) %>% 
+  ggsurvfit(linewidth = 0.8, type = "cumhaz", linetype_aes = TRUE, color = "cyan") +
+  add_risktable(risktable_stats = "n.risk",
+                risktable_height = 0.20) +
+  add_censor_mark() +
+  add_confidence_interval() +
+  theme_classic(base_size = 10) +
+  theme(legend.position='none') +
+  ggsci::scale_fill_nejm()
+
+library(patchwork)
+wrap_plots(ggsurvfit_build(p1)) +
+  inset_element(ggsurvfit_build(p2), 
+                left = 0.50, bottom = 0.55, right = 1.0, top = 1.0)
+
+
+
+
+
+
 
 
 
