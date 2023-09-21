bbcc73e2440b4eed87b9d15515d03c5a856a3f96
wane199 <wane199@outlook.com>
2023-09-21 21:42:28 +0800

    0921

--- single_cell_RNA-seq/chromosomes_map.R
+++ single_cell_RNA-seq/chromosomes_map.R
@@ -3,19 +3,33 @@
 ##### Install chromoMap #####
 install.packages("chromoMap")
 install.packages("RIdeogram")
-
+??chromosome_map
 ##### Prepare Input Data Files #####
 # chromoMap files for this vignette
 # chromosome files
-# chr_file_1 = "chr_file_without_centromere.txt"
-# chr_file_2 = "chr_file_with_centromere.txt"
+rm(list = ls())
 getwd()
-chr_file_1 <- read.table("C:\\Users\\wane\\Documents\\file\\sci\\Shuntak\\MR\\chr_file_without_centromere.csv", sep = ";")
-chr_file_2 <- read.table("C:\\Users\\wane\\Documents\\file\\sci\\Shuntak\\MR\\chr_file_with_centromere.csv", sep = ";")
+chr_file_1 = "./single_cell_RNA-seq/chr_file_without_centromere.txt"
+chr_file_2 = "./single_cell_RNA-seq/chr_file_with_centromere.txt"
+
 
 # annotation files
-anno_file_1 <- read.table("C:\\Users\\wane\\Documents\\file\\sci\\Shuntak\\MR\\annotation_pos.csv", sep = ";")
-anno_file_2 <- read.table("C:\\Users\\wane\\Documents\\file\\sci\\Shuntak\\MR\\annotation_pos_and_neg.csv", sep = ";")
+anno_file_1 <- "./single_cell_RNA-seq/annotation_pos.txt"
+anno_file_2 <- "./single_cell_RNA-seq/annotation_pos_and_neg.txt"
+
+df = data.frame(chr=c("chr16"),
+                start=2475127,end=c(2505730),cent=c(2495030))
+write.table(df,"C:\\Users\\wane\\Documents\\file\\sci\\Shuntak\\MR\\chromosome_file.txt",quote=F,
+            sep="\t",row.names=FALSE,col.names=FALSE)
+anno = data.frame(Ele=c("An1","An2"),
+                  chr=c("chr16","chr16"),start=c(2485127,2551127),
+                  end=c(2485573,2551730),score=c(10,177))
+write.table(anno,"C:\\Users\\wane\\Documents\\file\\sci\\Shuntak\\MR\\anno.txt",quote=F,
+            sep="\t",row.names=FALSE,col.names=FALSE)
+
+library(chromoMap)
+chromoMap("C:\\Users\\wane\\Documents\\file\\sci\\Shuntak\\MR\\chromosome_file.txt",
+          "C:\\Users\\wane\\Documents\\file\\sci\\Shuntak\\MR\\anno.txt")
 
 # R objects as chromoMap Inputs
 # passing data.frames directly instead of files
@@ -27,12 +41,15 @@ chromoMap(list(chr.data1, chr.data2),
 )
 
 # My first chromoMap
-library(chromoMap)
 chromoMap(chr_file_1, anno_file_1)
 
 # Now, let’s create one with centromeres.
 chromoMap(chr_file_2,anno_file_1)
 
+# Point and Segment-annotation plots
+chromoMap("./single_cell_RNA-seq/chr_file_with_centromere.txt",
+          "./single_cell_RNA-seq/annotation_pos_and_neg.txt",
+          segment_annotation = T)
 
 
 
